7f76f40583c06cd48909b8a119295379dc8efa47
about14sheep <aburger91@gmail.com>
2024-06-11 14:59:24 -0500

    use storage blob downloadToBufferOptions and remove custom error class

--- src/BlobReadstream.ts
+++ src/BlobReadstream.ts
@@ -1,45 +1,36 @@
-import type { BlobClient } from "@azure/storage-blob";
+import type { BlobClient, BlobDownloadToBufferOptions } from "@azure/storage-blob";
 import { Readable, type ReadableOptions } from "stream";
-import { BlobReadstreamError } from "./BlobReadstreamError";
-
-export type BlobReadstreamOptions = {
-  retry?: number;
-}
 
 export class BlobReadstream extends Readable {
   blobClient: BlobClient;
+  blobDownloadToBufferOptions: BlobDownloadToBufferOptions | undefined;
   curr: number = 0;
-  retry: number = 3;
 
   constructor(
     blobClient: BlobClient,
-    options?: BlobReadstreamOptions,
+    downloadToBufferOptions?: BlobDownloadToBufferOptions,
     nodeReadableOptions?: ReadableOptions
   ) {
     super(nodeReadableOptions);
     this.blobClient = blobClient;
-
-    if (options) {
-      const { retry = 3 } = options;
-      this.retry = retry;
-    }
+    this.blobDownloadToBufferOptions = downloadToBufferOptions;
+  
   }
 
   async _read(size: number): Promise<void> {
     try {
       const buff = Buffer.alloc(size);
-      await this.blobClient.downloadToBuffer(buff, this.curr, size);
+      await this.blobClient.downloadToBuffer(
+        buff,
+        this.curr,
+        size,
+        this.blobDownloadToBufferOptions
+      );
 
       this.curr += buff.byteLength;
       this.push(buff);
     } catch (error: unknown) {
-      if (this.retry > 0) {
-        this.retry--;
-      } else {
-        this.destroy(
-          new BlobReadstreamError("Max retries reached", error)
-        );
-      }
+        this.destroy(error as Error);
     }
   }
 }
--- src/BlobReadstreamError.ts
+++
@@ -1,7 +0,0 @@
-export class BlobReadstreamError extends Error {
-  BlobClientError: unknown;
-  constructor(message: string, blobClientError: unknown) {
-    super(message);
-    this.BlobClientError = blobClientError;
-  }
-}
