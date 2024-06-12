085bd952f1a96cde4df63279dda6b082fc4fbcc4
about14sheep <aburger91@gmail.com>
2024-06-12 08:52:24 -0500

    handle moving current cursor position by range

--- src/BlobReadstream.ts
+++ src/BlobReadstream.ts
@@ -1,36 +1,65 @@
 import type { BlobClient, BlobDownloadToBufferOptions } from "@azure/storage-blob";
 import { Readable, type ReadableOptions } from "stream";
 
-export class BlobReadstream extends Readable {
+export type BlobReadstreamParameters = {
   blobClient: BlobClient;
-  blobDownloadToBufferOptions: BlobDownloadToBufferOptions | undefined;
-  curr: number = 0;
+  contentLength: number;
+  byteRange: number;
+  downloadToBufferOptions?: BlobDownloadToBufferOptions;
+}
+
+export class BlobReadstream extends Readable {
+  private blobClient: BlobClient;
+  private blobDownloadToBufferOptions: BlobDownloadToBufferOptions | undefined;
+  private curr: number = 0;
+  private contentLength: number;
+  private range: number;
 
   constructor(
-    blobClient: BlobClient,
-    downloadToBufferOptions?: BlobDownloadToBufferOptions,
+    blobReadstreamParameters: BlobReadstreamParameters,
     nodeReadableOptions?: ReadableOptions
   ) {
     super(nodeReadableOptions);
+    const {
+      blobClient,
+      contentLength,
+      byteRange,
+      downloadToBufferOptions
+    } = blobReadstreamParameters;
+
     this.blobClient = blobClient;
+    this.contentLength = contentLength;
+    this.range = byteRange;
     this.blobDownloadToBufferOptions = downloadToBufferOptions;
-  
   }
 
-  async _read(size: number): Promise<void> {
-    try {
-      const buff = Buffer.alloc(size);
-      await this.blobClient.downloadToBuffer(
-        buff,
-        this.curr,
-        size,
-        this.blobDownloadToBufferOptions
-      );
+  async _read(): Promise<void> {
+    if (this.curr > this.contentLength) {
+      this.push(null);
+    } else {
+      try {
+        const requestRange = this.getRequestRange();
+        const buff = Buffer.alloc(requestRange);
+        await this.blobClient.downloadToBuffer(
+          buff,
+          this.curr,
+          requestRange,
+          this.blobDownloadToBufferOptions
+        );
 
-      this.curr += buff.byteLength;
-      this.push(buff);
-    } catch (error: unknown) {
+        this.curr += requestRange + 1;
+        this.push(buff);
+      } catch (error: unknown) {
         this.destroy(error as Error);
+      }
     }
+  }
+
+  getRequestRange() {
+    if (this.curr + this.range > this.contentLength) {
+      return this.contentLength - this.curr;
+    }
+
+    return this.range;
   }
 }
